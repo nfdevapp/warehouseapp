6c5b5932167899c3b93732640f82daa5fb123a7d
chris s <test@test.com>
2025-11-24 15:23:47 +0100

    added MongoDB

---
+++ backend/src/main/java/org/example/backend/repository/WarehouseRepo.java
@@ -0,0 +1,8 @@
+package org.example.backend.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WarehouseRepo extends MongoRepository {
+}
